560444123e047f43ee5f5bcbf44d9a575977ec1c
josuescardini <82621531+JosueScardini@users.noreply.github.com>
2021-06-20 03:27:51 +0000

    Criando função para mostrar resultados

--- circuitos.cpp
+++ circuitos.cpp
@@ -13,3 +13,16 @@ struct tCircuito {
     int i2 = V1/req2;
     int p1 = V1*i1;
     int p2 = V1*i2;
+
+//Criando função para mostrar os resultados
+    void mostra (){
+  
+  cout<<" A resistencia em serie é "<<req1<<endl;
+  cout<<" A resistencia em paralelo é "<<req2<<endl;
+  cout<<" A corrente em serie é "<<i1<<endl;
+  cout<<" A corrente em paralelo é "<<i2<<endl;
+  cout<<" A potência com resistências em serie é "<<p1<<endl;
+  cout<<" A potência com resistencias em paralelo é "<<p2<<endl;
+}
+
+};
